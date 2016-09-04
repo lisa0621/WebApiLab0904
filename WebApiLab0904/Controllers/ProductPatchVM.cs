3526fe78c0127610c1227b0b2c802b34241648e9
SHA-PAI LI <SHA-PAI LI>
2016-09-04 10:46:43 +0800

    Attribute Routing

--- WebApiLab0904/Controllers/ProductPatchVM.cs
+++ WebApiLab0904/Controllers/ProductPatchVM.cs
@@ -4,7 +4,7 @@ namespace WebApiLab0904.Controllers
 {
     public class ProductPatchVM
     {
-        public  Nullable<decimal> Price { get; set; }
+        public Nullable<decimal> Price { get; set; }
         public Nullable<decimal> Stock { get; set; }
     }
 }
