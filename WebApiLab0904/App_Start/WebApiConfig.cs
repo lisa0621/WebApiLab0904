c69af97597ea3111a07b97211b1010da32b73a4b
SHA-PAI LI <SHA-PAI LI>
2016-09-04 15:07:43 +0800

    JsonFormatter JsonIgnore

--- WebApiLab0904/App_Start/WebApiConfig.cs
+++ WebApiLab0904/App_Start/WebApiConfig.cs
@@ -13,10 +13,20 @@ namespace WebApiLab0904
     {
         public static void Register(HttpConfiguration config)
         {
+            var json = GlobalConfiguration.Configuration.Formatters.JsonFormatter;
+            
+            //排版 不太需要
+            json.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+
+            //駝峰式屬性名稱 js的一般屬性名稱是小寫開頭 對前端工程師比較友善
+            json.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+
             // Web API 設定和服務
             // 將 Web API 設定成僅使用 bearer 權杖驗證。
             config.SuppressDefaultHostAuthentication();
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+
             //將CheckModelStateAttribute加入到全站共用的ActionFilter
             //config.Filters.Add(new CheckModelStateAttribute());
 
